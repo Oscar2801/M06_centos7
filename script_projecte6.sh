d5048a26e006bb727bb51e597f64cd5a94355cf6
Oscar2801 <57793568+Oscar2801@users.noreply.github.com>
2020-11-09 17:58:55 +0100

    Add files via upload

---
+++ script_projecte6.sh
@@ -0,0 +1,69 @@
+https://github.com/Oscar2801/M06_centos7
+
+#!/bin/bash
+
+# This script creates a new user on local system.
+# You must supply a username as an argument to the script.
+# Optionally, you can also provode a comment for the acc as argument.
+# A password will be auto generated for account.
+# The username, password, and host for account will be displayed.
+
+# Make sure the script is being executed with superuser privileges.
+if [[ "${UID}" -ne 0 ]]
+then
+        echo 'Please run with sudo or as root.' >&2
+        exit 1
+fi
+
+# If they don't supply at least one argument, then give them help.
+if [[ "${#}" -lt 1 ]]
+then
+        echo "Usage: ${0} USER_NAME [COMMENT]..." >&2
+        echo 'Create an account on the local system with the name of USER_NAME and a comments field of COMMENT.' >&2
+        exit 1
+fi
+
+# The first parameter is user name
+USER_NAME="${1}"
+
+# The rest of the parameters are for accout comments.
+shift
+COMMENT="${@}"
+
+# Generate a password
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+
+# Create the user wtih teh password.
+useradd -c "${COMMENT}" -m ${USER_NAME} &> /dev/null
+
+# Check to see if the useradd command succeeded.
+# We don't want to tell the user that an account was created when it hasn't been.
+if [[ "${?}" -ne 0 ]]
+then
+        echo 'The account could not be created.' >&2
+        exit 1
+fi
+
+# Set the password.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME} &> /dev/null
+
+# Check to see if the password command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+        echo 'The password for the account could not be set.' >&2
+        exit 1
+fi
+
+# Force password change on first login.
+passwd -e ${USER_NAME} &> /dev/null
+
+# Display the username, password, and the host where the user was created.
+echo 'username'
+echo "${USER_NAME}"
+echo
+echo 'password'
+echo "${PASSWORD}"
+echo
+echo 'host: '
+echo "${HOSTNAME}"
+exit 0
